bc92561849fc3f6cf4a146380006fd5caa0b77b7
Bobby142 <73199022+Bobby142@users.noreply.github.com>
2022-12-20 14:51:11 -0800

    Create Xcode app with FDA api
    create Xcode app now we have the ability to pull the required info.

---
+++ Recall/Recall/RecallApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecallApp.swift
+//  Recall
+//
+//  Created by BOBBY ROOTS on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct RecallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
