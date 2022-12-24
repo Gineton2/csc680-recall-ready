6c2235f636b993b137e675ad83a723bff1c24cbf
Gineton Alencar II <Gineton2@users.noreply.github.com>
2022-12-23 20:53:01 -0800

    Add text and location button to first view

--- Recall/Views/FirstView.swift
+++ Recall/Views/FirstView.swift
@@ -6,10 +6,31 @@
 //
 
 import SwiftUI
+import CoreLocationUI
 
+// This is the first view that the user will see.
+// Show app Title "Recall Ready" and a button to get user location.
 struct FirstView: View {
+    @StateObject var locationService = LocationService()
+    
     var body: some View {
-        Text("Recall Ready")
+        VStack {
+            Text("Recall Ready")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.bottom, 10)
+            
+            Text("\tRecall Ready provides up-to-date information on food recall announcements by the FDA in a user's region. \r\tStay informed! Help keep you and your family safe from foodborne illness and other health risks.")
+                .font(.callout)
+                .italic()
+                .padding(50)
+            
+            LocationButton(.currentLocation){
+            }
+            .symbolVariant(.fill)
+            .labelStyle(.titleAndIcon)
+            .cornerRadius(25.0)
+        }
     }
 }
 
