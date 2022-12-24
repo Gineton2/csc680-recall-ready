0881eea3f288b1ac35fbddd08056aead71b3f357
Gineton Alencar II <Gineton2@users.noreply.github.com>
2022-12-23 21:57:36 -0800

    Update Product Recall model with more properties

--- Recall/Models/RecallsModel.swift
+++ Recall/Models/RecallsModel.swift
@@ -13,7 +13,30 @@ struct Response: Codable {
 }
 
 struct Product:Codable {
+    var status: String
+    
+    // location properties
+    // TODO: maybe implement as var location: ProductLocation?
+    var city: String
+    var state: String
+    var country: String
+    var address_1: String
+    var address_2: String
+    var postal_code: String
+    var distribution_pattern: String
+    
+    var classification: String
+    var product_type: String
+    var event_id: String
+    var recalling_firm: String
+    var voluntary_mandated: String
     var recall_number: String
-    var reason_for_recall: String
     var product_description: String
+    var reason_for_recall: String
+    
+    //dates
+    var recall_initiation_date: String
+    var center_classification_date: String
+    var report_date: String
+    
 }
