e6bbd037cf6a5554c3546cd48e67f33f2260a9b0
Gineton Alencar II <Gineton2@users.noreply.github.com>
2022-12-23 21:58:22 -0800

    Create non-functional ProductLocation model

---
+++ Recall/Models/ProductLocation.swift
@@ -0,0 +1,18 @@
+//
+//  ProductLocation.swift
+//  Recall
+//
+//  Created by Gineton Alencar II on 12/23/22.
+//
+
+//import Foundation
+//
+//struct ProductLocation: Codable {
+//    var city: String
+//    var state: String
+//    var country: String
+//    var address_1: String
+//    var address_2: String
+//    var postal_code: String
+//    var distribution_pattern: String
+//}
